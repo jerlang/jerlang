1de6a91318fe15e9c65fb65b99b1deb7a932ff4f
Constantin Rack <constantin@rack.li>
2015-10-01 07:59:39 +0200

    Problem: send opcode impl contains unnecessary code

--- src/main/java/org/jerlang/erts/emulator/op/Send.java
+++ src/main/java/org/jerlang/erts/emulator/op/Send.java
@@ -4,7 +4,6 @@ import org.jerlang.Module;
 import org.jerlang.Process;
 import org.jerlang.ProcessRegistry;
 import org.jerlang.erts.emulator.Instruction;
-import org.jerlang.type.Integer;
 import org.jerlang.type.List;
 import org.jerlang.type.PID;
 import org.jerlang.type.Term;
@@ -12,18 +11,15 @@ import org.jerlang.type.Term;
 /**
  * Send argument in x(1) as a message to the destination process in x(0).
  * The message in x(1) ends up as the result of the send in x(0).
- *
- * TODO: Original documentation says x(0) contains the message.
  */
 public class Send {
 
     public static Term execute(Process proc, Module m, Instruction i, List params) {
-        Term x = proc.getX(0);
         PID pid = proc.getX(0).toPID();
         Term message = proc.getX(1);
         Process destination = ProcessRegistry.resolve(pid);
         destination.send(message);
-        proc.setX(Integer.of(0), message);
+        proc.setX(0, message);
         return null;
     }
 
